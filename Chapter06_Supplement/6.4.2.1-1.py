2e9759c519394f178787a7a277f1d4ae5066c4b4
Andy <1303680675@qq.com>
2024-05-30 07:46:28 +0000

    upper

--- Chapter06_Supplement/6.4.2.1-1.py
+++ Chapter06_Supplement/6.4.2.1-1.py
@@ -19,3 +19,28 @@ def lookup(data,label,name):
 print(lookup(storage,"first","Anne"))
 print(lookup(storage,"middle","Lie"))
 print(lookup(storage,"last","Heland"))
+print("\n")
+def store(data,full_name):
+    names = full_name.split()
+    print(names)
+    if len(names) == 2:
+        names.insert(1,'')
+    labels = 'first','middle','last'
+    for label,name in zip(labels,names):
+        people = lookup(data,label,name)
+        if people:
+            print("名字已存在")
+            people.append(full_name)
+            print(people)
+        else:
+            print("名字不存在,存储名字")
+            data[label][name] = [full_name]
+            print(data)
+    print("\n\n")
+
+store(storage,"An Yang Fan")
+store(storage,"Chen shou xiang")
+store(storage,"An Yang Fan")
+
+
+
